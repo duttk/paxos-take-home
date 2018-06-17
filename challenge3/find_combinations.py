c61cf2f3feb212af616d5a7c60f6efcc76f1611b
Kaushik Dutt <duttk92@gmail.com>
2018-06-17 16:51:59 -0500

    fixing lint

--- challenge3/find_combinations.py
+++ challenge3/find_combinations.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import os
 import sys
 from itertools import product
 
@@ -16,16 +15,16 @@ def get_combinations(pattern):
     if len(pattern) == 1:
         if pattern[0] == 'X':
             return ['0', '1']
-        else:
-            return [pattern[0]]
-    
+
+        return [pattern[0]]
+
     all_patterns = []
     first_part = get_combinations(pattern[0])
     rest_part = get_combinations(pattern[1:])
 
     for first, rest in product(first_part, rest_part):
         all_patterns.append(first + rest)
-    
+
     return all_patterns
 
 if __name__ == '__main__':
@@ -38,7 +37,7 @@ if __name__ == '__main__':
     # Check if the PATTERN argument is correct
     if set(PATTERN) - set(['0', '1', 'X']):
         print('ERROR: The pattern can only contain 0, 1 and X')
-        sys.exit(1)        
+        sys.exit(1)  
 
     # Find all combinations for X replacement in the string
     combinations = get_combinations(PATTERN)
